34d142a9600e42f487ab2c6e3a6e51f5627669fc
Pablo Carrasco <pablo@webassist.pro>
2023-11-22 21:12:14 +0100

    functional socket connection

--- index.ts
+++ index.ts
@@ -1,41 +1,63 @@
 const args = process.argv.slice(2);
-const count = '--count' == args[0]; // Usar este flag para contar cantidad de objetos recibidos por segundo
+const count = "--count" == args[0]; // Usar este flag para contar cantidad de objetos recibidos por segundo
 
-import  path  from "node:path";
-import  net from "node:net";
+import net from "node:net";
 
-const express = require('express');
+const express = require("express");
+const { Server } = require("socket.io");
+const { createServer } = require("node:http");
+const { join } = require('node:path');
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
 const app = express();
+const clientServer = createServer(app);
+const io = new Server(clientServer, {
+  cors: {
+    origin: '*'
+  }
+});
+
 var n = 0;
 let clientdata = {};
 const server = net.createServer((socket) => {
-
-    socket.on('data', (data) => {
-      clientdata = JSON.parse(data as unknown as string);
-      if (!count){
-        return JSON.stringify(data, null,"   ");
-      }
-      else{
-        n+=1;
-      }
-    });
+  socket.on("data", (data) => {
+    clientdata = JSON.parse(data as unknown as string);
+    eventEmitter.emit('data-received');
+    if (!count) {
+      return JSON.stringify(data, null, "   ");
+    } else {
+      n += 1;
+    }
+  });
 });
 
 server.listen(8989, () => {
-  console.log('Server started on port 8989');
+  console.log("Server started on port 8989");
 });
-if (count){
-    setInterval(() => {
-    return n; 
-    n=0;
+if (count) {
+  setInterval(() => {
+    return n;
+    n = 0;
   }, 1000);
 }
 
-app.get('/', (req:any, res:any) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end(JSON.stringify(clientdata));  
+app.get("/", (req: any, res: any) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end(JSON.stringify(clientdata));
 });
-app.listen(3000);
-console.log('Client server started at port 3000');
+
+app.get('/', (req:any, res:any) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+clientServer.listen(3000);
+console.log("Client server started at port 3000");
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+  eventEmitter.on('data-received', () => {
+    socket.emit('data-parsed', clientdata);
+  });
+});
