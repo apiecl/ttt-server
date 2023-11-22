2901c0e7de81d0e051facc3774b187e5b09254e1
Pablo Carrasco <pablo@webassist.pro>
2023-11-22 15:38:59 +0100

    ts-node and functional server

---
+++ index.ts
@@ -0,0 +1,42 @@
+const args = process.argv.slice(2);
+const count = '--count' == args[0]; // Usar este flag para contar cantidad de objetos recibidos por segundo
+
+import  path  from "node:path";
+import  net from "node:net";
+
+const express = require('express');
+
+const app = express();
+var n = 0;
+let clientdata = {};
+const server = net.createServer((socket) => {
+
+    socket.on('data', (data) => {
+      clientdata = JSON.parse(data as unknown as string);
+      //console.log(clientdata);
+      if (!count){
+        return JSON.stringify(data, null,"   ");
+      }
+      else{
+        n+=1;
+      }
+    });
+});
+
+server.listen(8989, () => {
+  console.log('Server started on port 8989');
+});
+if (count){
+    setInterval(() => {
+    return n; 
+    n=0;
+  }, 1000);
+}
+
+app.get('/', (req:any, res:any) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.end(JSON.stringify(clientdata));  
+});
+app.listen(3000);
+console.log('Client server started at port 3000');
--- src/config.ts
+++
@@ -1,9 +0,0 @@
-interface config {
-    authorized_origin_ttt: string,
-    authorized_origin_sensors: string
-}
-
-export const config = {
-    authorized_origin_ttt: "http://127.0.0.1:5173",
-    authorized_origin_sensors: "http://127.0.0.1:8989",
-}
